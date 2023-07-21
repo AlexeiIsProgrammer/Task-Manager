cf3b3b36de447d69c99df10c4cf285638e737e6e
Alexei <avenge.now.r@mail.ru>
2023-07-21 10:32:14 +0300

    feat: add deploy config, add tailwind to project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header"></header>
+            <header className="App-header underline">Пока</header>
         </div>
     );
 }
